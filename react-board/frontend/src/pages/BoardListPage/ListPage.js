3b808dc2371cb9b8467b26cf850eee8fd2a0be48
Vida0822 <sinhimin11@naver.com>
2025-11-13 15:55:43 +0900

    feat: 페이지 생성

---
+++ react-board/frontend/src/pages/BoardListPage/ListPage.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import './ListPage.css' ; 
+
+const ListPage = () => {
+    return(
+        <div class="list-page-container">
+            게시판 목록 출력
+        </div>
+    )
+} ; 
+
+export default ListPage ; 
